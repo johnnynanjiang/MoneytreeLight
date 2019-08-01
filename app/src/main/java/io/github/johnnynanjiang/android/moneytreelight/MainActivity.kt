59aee681d64926491b165b9554a00ac3eec414ea
Nan Jiang <johnny.nan.jiang@gmail.com>
2019-08-01 08:58:41 +0800

    Initial commit

---
+++ app/src/main/java/io/github/johnnynanjiang/android/moneytreelight/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.johnnynanjiang.android.moneytreelight
+
+import android.support.v7.app.ActionBarActivity
+import android.os.Bundle
+
+class MainActivity : ActionBarActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
