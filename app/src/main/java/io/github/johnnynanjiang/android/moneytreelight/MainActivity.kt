8631f3a6acf0aeb95c89409436e6854d723e90fe
Nan Jiang <johnny.nan.jiang@gmail.com>
2019-08-01 09:13:14 +0800

    Upgrade to androidx

--- app/src/main/java/io/github/johnnynanjiang/android/moneytreelight/MainActivity.kt
+++ app/src/main/java/io/github/johnnynanjiang/android/moneytreelight/MainActivity.kt
@@ -1,9 +1,9 @@
 package io.github.johnnynanjiang.android.moneytreelight
 
-import android.support.v7.app.ActionBarActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ActionBarActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
