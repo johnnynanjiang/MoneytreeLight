bae80fe0bf81ac17f62dee2e35acf98d776808fc
Nan Jiang <johnny.nan.jiang@gmail.com>
2019-08-06 10:24:08 +0800

    Implement app color and back on actionbar

--- app/src/main/java/io/github/johnnynanjiang/android/moneytreelight/view/MainActivity.kt
+++ app/src/main/java/io/github/johnnynanjiang/android/moneytreelight/view/MainActivity.kt
@@ -3,11 +3,22 @@ package io.github.johnnynanjiang.android.moneytreelight.view
 import android.os.Bundle
 import com.airbnb.mvrx.BaseMvRxActivity
 import io.github.johnnynanjiang.android.moneytreelight.R
+import android.view.MenuItem
 
 class MainActivity : BaseMvRxActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            android.R.id.home -> {
+                supportFragmentManager.popBackStack()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
 }
